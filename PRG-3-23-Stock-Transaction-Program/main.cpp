a5bb1ae0eef7a4c683bdd1bf98c650d7496e1d8d
Keith Smith <aeiouandsometimesy@me.com>
2017-10-07 08:08:46 -0400

    Initial Commit

---
+++ PRG-3-23-Stock-Transaction-Program/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PRG-3-23-Stock-Transaction-Program
+//
+//  Created by Keith Smith on 10/7/17.
+//  Copyright © 2017 Keith Smith. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
